672c1eb0982c47b3d1aab0c623394af60003fb4f
Linh Nguyen <xiahii123@gmail.com>
2023-03-13 09:17:27 +0700

    Update Base Test headless

--- actions/commons/BaseTest.java
+++ actions/commons/BaseTest.java
@@ -6,24 +6,50 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.opera.OperaDriver;
 
 public class BaseTest {
 	private WebDriver driverBaseTest;
 	private String projectPath = System.getProperty("user.dir");
+
 	protected WebDriver getBrowserDriver(String browserName) {
-		if(browserName.equals("firefox")) {
+		if (browserName.equals("firefox")) {
 			System.setProperty("webdriver.gecko.driver", projectPath + "\\browserDrivers\\geckodriver.exe");
 			driverBaseTest = new FirefoxDriver();
-			} else if(browserName.equals("chrome")) {
+			// Headless dung cho Frontend de run Unit Test/Data Analyst/MMO (Crawl Data)
+			// Headless chạy ngầm, k bật giao diện trình duyệt lên
+		} else if (browserName.equals("h_firefox")) {
+			System.setProperty("webdriver.gecko.driver", projectPath + "\\browserDrivers\\geckodriver.exe");
+			
+			// Browser Option: selenium 3.xx
+			FirefoxOptions options = new FirefoxOptions();
+			options.addArguments("-headless");
+			options.addArguments("window-size=1920x1080");
+			driverBaseTest = new FirefoxDriver(options);
+			
+		} else if (browserName.equals("chrome")) {
 			System.setProperty("webdriver.chrome.driver", projectPath + "\\browserDrivers\\chromedriver.exe");
 			driverBaseTest = new ChromeDriver();
-			} else if(browserName.equals("edge")) {
+		} else if (browserName.equals("h_chrome")) {
+			System.setProperty("webdriver.chrome.driver", projectPath + "\\browserDrivers\\chromedriver.exe");
+			driverBaseTest = new ChromeDriver();
+		} else if (browserName.equals("edge")) {
 			System.setProperty("webdriver.edge.driver", projectPath + "\\browserDrivers\\msedgedriver.exe");
 			driverBaseTest = new EdgeDriver();
-			} else {
-				throw new RuntimeException("Browser name invalid");
-			}
-		
+		} else if (browserName.equals("h_edge")) {
+			System.setProperty("webdriver.edge.driver", projectPath + "\\browserDrivers\\msedgedriver.exe");
+			driverBaseTest = new EdgeDriver();
+		} else if (browserName.equals("opera")) {
+			System.setProperty("webdriver.opera.driver", projectPath + "\\browserDrivers\\operadriver.exe");
+			driverBaseTest = new OperaDriver();
+		} else if (browserName.equals("h_opera")) {
+			System.setProperty("webdriver.opera.driver", projectPath + "\\browserDrivers\\operadriver.exe");
+			driverBaseTest = new OperaDriver();
+		} else {
+			throw new RuntimeException("Browser name invalid");
+		}
+
 		driverBaseTest.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
 		driverBaseTest.get("https://demo.nopcommerce.com/");
 		return driverBaseTest;
