dafd1b3ba6e99e21eb58b0e66b96f2f350255e38
Linh Nguyen <xiahii123@gmail.com>
2023-03-28 15:10:43 +0700

    basepageUI update

--- interfaces/pageUIs/nopCommerce/user/BasePageUI.java
+++ interfaces/pageUIs/nopCommerce/user/BasePageUI.java
@@ -5,5 +5,6 @@ public class BasePageUI {
 	public static final String MY_PRODUCT_REVIEW_LINK = "//div[contains(@class,'account-navigation')]//a[text()='My product reviews']";
 	public static final String REWARD_POINT_LINK = "//div[contains(@class,'account-navigation')]//a[text()='Reward points']";
 	public static final String CUSTOMER_INFO_LINK = "//div[contains(@class,'account-navigation')]//a[text()='Customer info']";
-
+	public static final String LOGOUT_LINK_AT_USER = "//a[@class='ico-logout']";
+	public static final String LOGOUT_LINK_AT_ADMIN = "//a[text()='Logout']";
 }
